c8bffc738409cb18c868960510a669dace87cb3b
Andre Almeida <andre.almeida@msn.com>
2015-10-17 16:53:17 -0300

    Create showIfWeAreNotInDanger
    create showIfWeAreNotInDanger return false

--- Test/LionTest.java
+++ Test/LionTest.java
@@ -7,13 +7,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class LionTest {
     private Lion lion;
     private Lion lionA;
-    private  Lion lionB;
+    private Lion lionB;
+    private  Lion lionAFalse;
 
 
     @Before
     public void setUp() throws Exception {
         this.lion = new Lion(true);
         this.lionA = new Lion(true);
+        this.lionAFalse = new Lion(false);
+
         this.lionB = new Lion(true);
     }
 
@@ -28,6 +31,15 @@ public class LionTest {
     public void showIfWeAreInDanger(){
         assertThat(lion.weAreInDanger(lionA, lionB), is(true));
     }
+
+
+    @Test
+    public void showIfWeAreNotInDanger(){
+        assertThat(lion.weAreInDanger(lionAFalse, lionB), is(false));
+    }
+
+
+
 
 
 
