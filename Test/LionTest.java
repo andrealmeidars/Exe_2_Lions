ec2757f6c4145b7302a380ed170f39501645dba9
Andre Almeida <andre.almeida@msn.com>
2015-10-17 16:21:49 -0300

    Create constructor in Lion
    Create construtor method in class Lion recieved boolean value

--- Test/LionTest.java
+++ Test/LionTest.java
@@ -1,5 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LionTest {
+    private Lion lion;
 
-    
+
+    @Before
+    public void setUp() throws Exception {
+        this.lion = new Lion(true);
+    }
+
+
+
+    @Test
+    public void showIfLionIsSmile(){
+       assertThat(lion.getSmile(), is(true));
+    }
+
+
+
+
+
+
 }
